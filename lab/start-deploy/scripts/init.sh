0e58d5bb10ca73ccb393f66ae5f6f1625d238787
gwind <lijian.gnu@gmail.com>
2018-11-05 20:08:20 +0800

    update deploy

--- lab/start-deploy/scripts/init.sh
+++ lab/start-deploy/scripts/init.sh
@@ -3,7 +3,7 @@
 #VALUE_FILE=../lab/start-deploy/config/authn/keys/public_key.pem
 #$ETCDSET /ga/middleware/uid/public_key ${VALUE_FILE} --value-is-file
 
-APP_ID=c13ec57b-2af8-4884-827e-dee5a6a6f841
+APP_ID=c025cb97-9cba-4a3d-aff9-7c5a66c967aa
 
 export ETCDCTL_API=3
 cat ../config/authn/keys/public_key.pem | etcdctl put /ga/middleware/uid/public_key
@@ -14,6 +14,7 @@ cat ../services/service/schema.yml | etcdctl put /ga/service/service/openapi/spe
 # add roles to permission
 etcdctl put ga.auth.permissions.authn:post:/app_token.roles '["authenticated"]'
 etcdctl put ga.auth.permissions.service:post:/service.roles '["admin"]'
+etcdctl put ga.auth.permissions.authz:post:/role/permission/append.roles '["authenticated"]'
 
 # add roles to user
 etcdctl put ga.auth.users.${APP_ID}.roles '["admin"]'
